6d6af3e8227fea384340fabd7fc33810da1bd4d1
joao-luca-016 <thezinluca15@gmail.com>
2026-06-11 16:33:53 -0300

    Update 08_anamnese.sql

--- sql/08_anamnese.sql
+++ sql/08_anamnese.sql
@@ -1,1 +1,21 @@
+CREATE TABLE anamneses (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 
+    cliente_id UUID NOT NULL,
+
+    data_anamnese TIMESTAMP DEFAULT NOW(),
+
+    hipertensao BOOLEAN DEFAULT FALSE,
+    diabetes BOOLEAN DEFAULT FALSE,
+    gestante BOOLEAN DEFAULT FALSE,
+
+    alergias TEXT,
+    medicamentos_em_uso TEXT,
+
+    historico_cirurgico TEXT,
+    observacoes TEXT,
+
+    CONSTRAINT fk_anamnese_cliente
+        FOREIGN KEY (cliente_id)
+        REFERENCES clientes(id)
+);
