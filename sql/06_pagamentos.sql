ecc817cc9ade9d6fc66ceb361fe8989734f136e8
joao-luca-016 <thezinluca15@gmail.com>
2026-06-11 16:32:33 -0300

    Update 06_pagamentos.sql

--- sql/06_pagamentos.sql
+++ sql/06_pagamentos.sql
@@ -1,1 +1,17 @@
+create table pagamentos (
+    id uuid primary key default gen_random_uuid(),
 
+    agendamento_id uuid not null,
+
+    valor decimal(10,2) not null,
+
+    forma_pagamento varchar(50),
+
+    data_pagamento timestamp default now(),
+
+    status varchar(20) default 'pago',
+
+    constraint fk_agendamento_pagamento
+        foreign key (agendamento_id)
+        references agendamentos(id)
+);
