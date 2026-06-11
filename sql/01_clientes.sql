c918b3ba82efe44e907e41b27ae6fc0f34c190c5
joao-luca-016 <thezinluca15@gmail.com>
2026-06-11 16:30:08 -0300

    Update 01_clientes.sql

--- sql/01_clientes.sql
+++ sql/01_clientes.sql
@@ -1,1 +1,11 @@
-
+create table clientes (
+    id uuid primary key default gen_random_uuid(),
+    nome varchar(200) not null,
+    cpf varchar(14) unique,
+    telefone varchar(20),
+    email varchar(200),
+    data_nascimento date,
+    endereco text,
+    observacoes text,
+    created_at timestamp default now()
+);
