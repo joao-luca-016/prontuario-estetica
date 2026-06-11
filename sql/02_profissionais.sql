17f84f1782af2b167d9fbc0337af35cbc0c1ea94
joao-luca-016 <thezinluca15@gmail.com>
2026-06-11 16:30:52 -0300

    Update 02_profissionais.sql

--- sql/02_profissionais.sql
+++ sql/02_profissionais.sql
@@ -1,1 +1,9 @@
-
+create table profissionais (
+    id uuid primary key default gen_random_uuid(),
+    nome varchar(200) not null,
+    conselho varchar(50),
+    especialidade varchar(100),
+    telefone varchar(20),
+    email varchar(200),
+    created_at timestamp default now()
+);
