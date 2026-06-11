61a238ecd0f388a62f71337f79adf1213096384a
joao-luca-016 <thezinluca15@gmail.com>
2026-06-11 16:33:30 -0300

    Update 07_usuarios.sql

--- sql/07_usuarios.sql
+++ sql/07_usuarios.sql
@@ -1,1 +1,13 @@
+CREATE TABLE usuarios (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 
+    nome VARCHAR(200) NOT NULL,
+    email VARCHAR(200) UNIQUE NOT NULL,
+    senha_hash TEXT NOT NULL,
+
+    perfil VARCHAR(50) NOT NULL,
+    
+    ativo BOOLEAN DEFAULT TRUE,
+
+    created_at TIMESTAMP DEFAULT NOW()
+);
