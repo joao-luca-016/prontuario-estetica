35b5748ffe98b638a85e8328dfa5ca79ea89f0bb
joao-luca-016 <thezinluca15@gmail.com>
2026-06-11 16:32:07 -0300

    Update 05_evolucoes.sql

--- sql/05_evolucoes.sql
+++ sql/05_evolucoes.sql
@@ -1,1 +1,18 @@
+create table evolucoes (
+    id uuid primary key default gen_random_uuid(),
 
+    cliente_id uuid not null,
+    profissional_id uuid not null,
+
+    data_evolucao timestamp default now(),
+
+    descricao text not null,
+
+    constraint fk_cliente_evolucao
+        foreign key (cliente_id)
+        references clientes(id),
+
+    constraint fk_profissional_evolucao
+        foreign key (profissional_id)
+        references profissionais(id)
+);
